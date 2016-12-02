61f63e7c19adddaaae380af223bba92c4a316a42
Daniel Arellano <danny@Daniels-MacBook-Pro.local>
2016-12-02 15:18:39 -0800

    Made helloapi easier to understand

--- hello-world-api/app.js
+++ hello-world-api/app.js
@@ -10,16 +10,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.all('/api/v1/hello', function(req, res) {
-  let gatewayMsg = req.headers['gateway-message'];
-  if(gatewayMsg) {
-    gatewayMsg = JSON.parse(gatewayMsg);
-  }
-  res.json({
+  const response = {
     message: 'hello',
     query: req.query,
     body: req.body,
-    'gateway-message': gatewayMsg
-  });
+  };
+
+  let gatewayMsg = req.headers['gateway-message'];
+  if(gatewayMsg) {
+    response['gateway-message'] = JSON.parse(gatewayMsg);
+  }
+
+  res.json(response);
 });
 
 module.exports = app;
