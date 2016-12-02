27e645669594612e36bf761aaa5508c32d44fe48
Daniel Arellano <danny@Daniels-MacBook-Pro.local>
2016-12-02 14:57:42 -0800

    Changes vars to const/let

--- hello-world-api/app.js
+++ hello-world-api/app.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
