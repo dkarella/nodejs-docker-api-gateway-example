c63d9eef179dd708d5a9a91ba5ad19088c7d9bdb
Daniel Arellano <danny@Daniels-MacBook-Pro.local>
2016-12-06 15:40:19 -0800

    added entry route

--- hello-world-api/app.js
+++ hello-world-api/app.js
@@ -9,6 +9,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get('/', function(req,res){
+  res.json({
+    hello: 'world!'
+  });
+});
+
 app.all('/api/v1/hello', function(req, res) {
   const response = {
     message: 'hello',
